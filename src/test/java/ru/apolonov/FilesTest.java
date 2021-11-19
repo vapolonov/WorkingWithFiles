b3a7ab5d6830526444a9c76c3d3d46ffff58f7a4
Vasiliy <vasvap@gmail.com>
2021-11-19 14:59:24 +0300

    pdf_xls_csv_zip tests added

--- src/test/java/ru/apolonov/FilesTest.java
+++ src/test/java/ru/apolonov/FilesTest.java
@@ -1,19 +1,24 @@
 package ru.apolonov;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilesTest {
 
@@ -42,8 +47,8 @@ public class FilesTest {
         open("https://github.com/junit-team/junit5/blob/main/README.md");
         File download = $("#raw-url").download();
         String fileContent = IOUtils.toString(new FileReader(download));
-        Assertions.assertTrue((fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_.")));
-        //Если элемент загрухкт файла (кнопка) не содержит аттрибут href
+        assertTrue((fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_.")));
+        //Если элемент загрузки файла (кнопка) не содержит аттрибут href
         //то можно использовать другой способ: selenide запустит настоящий прокси-сервер между нашим кодом и браузером,
         // который будет пытаться отловить трафик и вычислить ссылку с href
         //не рекомендуется делать т.к. тесты становяться нестабильными
@@ -55,11 +60,46 @@ public class FilesTest {
 
     @Test
     @DisplayName("Скачивание pfd-файла")
-    void pdfFileDownloadTest() throws FileNotFoundException {
+    void pdfFileDownloadTest() throws IOException {
         open("https://junit.org/junit5/docs/current/user-guide/");
-        File pdf = $(By.linkText("PDF download")).download();
-
-
+        File pdf = $(byText("PDF download")).download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals(164, parsedPdf.numberOfPages);
     }
 
+    @Test
+    @DisplayName("Скачивание xls-файла")
+    void xlsFileDownloadTest() throws IOException {
+        open("http://romashka2008.ru/");
+        File xls = $$("a[href*='prajs']").find(text("Скачать Прайс-лист Excel")).download();
+        XLS parsedXls = new XLS(xls);
+        boolean checkPassed = parsedXls.excel.getSheetAt(0).getRow(10).getCell(1).getStringCellValue().contains("ООО \"Ромашка\"");
+        assertTrue(checkPassed);
+    }
+
+    @Test
+    @DisplayName("Парсинг CSV файлов")
+    void parseCsvFileTest() throws IOException, CsvException {
+        ClassLoader classloader = this.getClass().getClassLoader();
+        try (InputStream is = classloader.getResourceAsStream("csv.csv");
+             Reader reader = new InputStreamReader(is)) {
+            CSVReader csvReader = new CSVReader(reader);
+            List<String[]> strings = csvReader.readAll();
+            assertEquals(strings.size(), 3);
+        }
+    }
+
+    @Test
+    @DisplayName("Парсинг ZIP файлов")
+    void parseZipFileTest() throws IOException {
+        ClassLoader classloader = this.getClass().getClassLoader();
+        try (InputStream is = classloader.getResourceAsStream("zip_2MB.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+        }
+    }
 }
+
